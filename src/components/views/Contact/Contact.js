597e65a99dde3228cc45001fc7c939b271d232e4
Karolina Jurczak <inka_c@tlen.pl>
2020-10-14 12:42:56 +0200

    Add content to contact page

--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -2,11 +2,58 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import styles from './Contact.module.scss';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import DescriptionIcon from '@material-ui/icons/Description';
+import EmailIcon from '@material-ui/icons/Email';
 
 const Contact = () => (
-  <div className={styles.root}>
-    <h2>Contact</h2>
-  </div>
+  <div>
+  <Container className={styles.root}>
+    <div className={styles.textWrapper}>
+      <h2 className={styles.title}>Kontakt</h2>
+      <div className={styles.icons}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <div className={styles.paper}>
+              <a href='https://drive.google.com/file/d/1N_qvLC0CYtz4ttsK4RgZkgqvuis84huv/view?usp=sharing' target='_blank' rel='noopener noreferrer'>
+                <span>Curriculum Vitae</span>
+                <DescriptionIcon>CV</DescriptionIcon>
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className={styles.paper}>
+              <a href='mailto:karolinajurczakk@gmail.com' target='_blank' rel='noopener noreferrer'>
+                <span>Email</span>
+                <EmailIcon>Email</EmailIcon>
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className={styles.paper}>
+              <a href='https://www.linkedin.com/in/karolina-jurczak/' target='_blank' rel='noopener noreferrer'>
+                <span>LinkedIn</span>
+                <LinkedInIcon>LinkedIn</LinkedInIcon>
+              </a>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className={styles.paper}>
+              <a href='https://github.com/KaJurczak/Portfolio' target='_blank' rel='noopener noreferrer'>
+                <span>GitHub</span>
+                <GitHubIcon>GitHub</GitHubIcon>
+              </a>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  </Container>
+</div>
+
 );
 
 Contact.propTypes = {
--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => (
         Trafiłeś na stronę przedstawiającą najważniejsze informacje o mnie. <br></br>
         Od półtora roku napędza mnie chęć zmiany swojej ścieżki zawodowej na front-end. Niedawno zakończyłam obszerny kurs programowania - Web Developer Plus w Kodilli. Przy kolejnych projektach korzystam nieustannie z morza wiedzy onlinowej:). <br></br>
         Dotychczas moja ścieżka zawodowa związana była z inżynierskimi tematami, jednak jest w niej parę wspólnych cech, które zachęciły mnie do pójścia w stronę budowy front-endu. <br></br>
-        Przede wszystkim lubię aby moja praca przynosiła efekt, aby po jej zakończeniu powstał &quot;żywy&quot; produkt. I osiągam to przez miliony małych kroczków, cierpliwość, a czasem przebrnięcie przez dość zaskakujące problemy. Mam doświadczenie w pracy w zespole, wiem że deadline to słowo klucz a klient (i grafik:) ma prawie zawsze rację... <br></br>
+        Przede wszystkim lubię aby moja praca przynosiła efekt, aby po jej zakończeniu powstał &quot;żywy&quot; produkt. I osiągam to przez dziesiątki małych kroczków, cierpliwość, a czasem przebrnięcie przez dość zaskakujące problemy. Mam doświadczenie w pracy w zespole, wiem że deadline to słowo klucz a klient (i grafik:) ma prawie zawsze rację... <br></br>
         W swojej dotychczasowej ścieżce zawodowej konieczne było ciągłe poszerzanie wiedzy, a na ostatnim stanowisku (asystenta projektanta) poznawanie nowych narzędzi i technologii. <br></br>
         </p>
         <h3 className={styles.subtitle}>Obecnie szukam swojej pierwszej pracy w IT.</h3>
