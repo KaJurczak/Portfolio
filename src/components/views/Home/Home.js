b36c289c2256ae7d58f91903e947d5524041bef2
Karolina Jurczak <inka_c@tlen.pl>
2020-10-14 10:58:08 +0200

    Add style to home page

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -7,39 +7,17 @@ import Container from '@material-ui/core/Container';
 const Home = () => (
   <div>
     <Container className={styles.root}>
-      <h2>Witaj</h2>
-      <p className={styles.text}>
-      Trafiłeś na stronę przedstawiającą najważniejsze informacje o mnie. <br></br>
-      Jestem Karolina. <br></br>
-      Od półtora roku napędza mnie chęć zmiany swojej ścieżki zawodowej na front-end. Niedawno zakończyłam obszerny kurs programowania - Web Developer Plus w Kodilli. Poza tym przy kolejnych projekcikach korzystam nieustannie z morza wiedzy onlinowej:).
-      Dotychczas moja ścieżka zawodowa związana była z inżynierskimi tematami, jednak jest w niej parę wspólnych cech, które zachęciły mnie do pójścia w stronę budowy front-endu. <br></br>
-      Przede wszystkim lubię aby moja praca przynosiła efekt, aby po jej zakończeniu powstał &quot;żywy&quot; produkt. I wiem że osiągnięcie tego wymaga miliona małych kroczków, cierpliwości, a czasem przebrnięcia przez dość zaskakujące problemy. Mam doświadczenie w pracy w zespole, wiem że deadline to słowo klucz a klient (i grafik;) ma prawie zawsze rację. <br></br>
-      W swojej dotychczasowej ścieżce zawodowej konieczne było ciągłe poszerzanie wiedzy, a na ostatnim stanowisku (asystenta projektanta) poznawanie nowych narzędzi i technologii. <br></br>
-      Obecnie szukam swojej pierwszej pracy w IT. 
-      </p>
-      <br></br>
-      {/* <h2>Wybrane produkty</h2> */}
-      <div className={styles.l}>
-        {/* {plants.length !== 0 ? (
-          <GridList cellHeight={180} className={classes.gridList}>
-            {plants.map((plant) => (
-              <GridListTile key={plant._id} className={styles.plantWrapper} component={Link} to={`/plant/${plant._id}`}>
-                <img src={plant.photo[0]} alt={plant.polishName} />
-                <GridListTileBar
-                  title={plant.polishName}
-                  subtitle={<span>price: {plant.price}PLN</span>}
-                  actionIcon={
-                    <IconButton aria-label={`info about ${plant.polishName}`} className={classes.icon}>
-                      <InfoIcon />
-                    </IconButton>
-                  }
-                />
-              </GridListTile>
-            ))}
-          </GridList>
-        ) : (
-          <p>Galeria już wkrótce</p>
-        )} */}
+      <div className={styles.textWrapper}>
+        <h2 className={styles.title}>Witaj</h2>
+        <p className={styles.text}>
+        Jestem Karolina. <br></br>
+        Trafiłeś na stronę przedstawiającą najważniejsze informacje o mnie. <br></br>
+        Od półtora roku napędza mnie chęć zmiany swojej ścieżki zawodowej na front-end. Niedawno zakończyłam obszerny kurs programowania - Web Developer Plus w Kodilli. Przy kolejnych projektach korzystam nieustannie z morza wiedzy onlinowej:). <br></br>
+        Dotychczas moja ścieżka zawodowa związana była z inżynierskimi tematami, jednak jest w niej parę wspólnych cech, które zachęciły mnie do pójścia w stronę budowy front-endu. <br></br>
+        Przede wszystkim lubię aby moja praca przynosiła efekt, aby po jej zakończeniu powstał &quot;żywy&quot; produkt. I wiem że osiągnięcie tego wymaga miliona małych kroczków, cierpliwości, a czasem przebrnięcia przez dość zaskakujące problemy. Mam doświadczenie w pracy w zespole, wiem że deadline to słowo klucz a klient (i grafik:) ma prawie zawsze rację... <br></br>
+        W swojej dotychczasowej ścieżce zawodowej konieczne było ciągłe poszerzanie wiedzy, a na ostatnim stanowisku (asystenta projektanta) poznawanie nowych narzędzi i technologii. <br></br>
+        </p>
+        <h3 className={styles.subtitle}>Obecnie szukam swojej pierwszej pracy w IT.</h3>
       </div>
     </Container>
   </div>
