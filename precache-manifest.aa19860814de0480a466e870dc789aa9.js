1e4b0a352de393ca9d3d895ce8204d180c4f2eaf
Karolina Jurczak <inka_c@tlen.pl>
2020-10-20 13:17:21 +0200

    Updates

---
+++ precache-manifest.aa19860814de0480a466e870dc789aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3739209c8edf28548784bf4d4877bb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "07efa6bed8152c2df97a",
+    "url": "/Portfolio/static/css/main.5c66065f.chunk.css"
+  },
+  {
+    "revision": "43863593e775b71148b5",
+    "url": "/Portfolio/static/js/2.cddef191.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.cddef191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07efa6bed8152c2df97a",
+    "url": "/Portfolio/static/js/main.e67fa206.chunk.js"
+  },
+  {
+    "revision": "32c071d202c1c28133cc",
+    "url": "/Portfolio/static/js/runtime-main.1228a781.js"
+  }
+]);
